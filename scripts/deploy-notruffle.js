7062f5c0d7054fa0ccd3bc0654424f148cbb97cf
noot <elizabethjbinks@gmail.com>
2020-05-04 17:42:49 -0400

    script done; calling ensFactory not working

--- scripts/deploy-notruffle.js
+++ scripts/deploy-notruffle.js
@@ -10,6 +10,7 @@ const DAOFactory = require('../build/contracts/DAOFactory')
 const APMRegistryFactory = require('../build/contracts/APMRegistryFactory')
 const ENS = require('../build/contracts/ENS')
 const ENSFactory = require('../build/contracts/ENSFactory')
+const EVMScriptRegistryFactory = require('../build/contracts/EVMScriptRegistryFactory')
 
 const ACL = require('../build/contracts/ACL')
 const Kernel = require('../build/contracts/Kernel')
@@ -26,17 +27,19 @@ const tldHash = namehash(tldName)
 const labelHash = '0x'+keccak256(labelName)
 const apmNode = namehash(`${labelName}.${tldName}`)
 
+const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+
 // async function getCurrentAccount() {
 //   const currentAccounts = await web3.eth.getAccounts();
 //   console.log("Unlocked account address: \t", currentAccounts[0]);
 //   return currentAccounts[0];
 // }
 
-async function deployContract(contractData, sender) {
+async function deployContract(contractData, sender, args=[]) {
   const testContract = new web3.eth.Contract(contractData.abi);
   return testContract
     .deploy({
-      arguments: ["1.0"],
+      arguments: args,
       data: contractData.bytecode
     })
     .send({
@@ -59,7 +62,21 @@ async function deployContract(contractData, sender) {
 }
 
 async function deployDaoFactory(from) {
+	let kernelBase = await deployContract(Kernel, from, [true])
+	let aclBase = await deployContract(ACL, from)
+	let evmScriptRegistryFactory = await deployContract(EVMScriptRegistryFactory, from)
+	let daoFactory = await deployContract(DAOFactory, from,    
+		[kernelBase.options.address,
+	    aclBase.options.address,
+	    evmScriptRegistryFactory ? evmScriptRegistryFactory.options.address : ZERO_ADDR
+    ])
 
+    return {
+      aclBase,
+      daoFactory,
+      evmScriptRegistryFactory,
+      kernelBase,
+    }
 }
 
 async function deploy() {
@@ -82,12 +99,30 @@ async function deploy() {
 		console.log('Missing ENS! Deploying a custom ENS...')
 
 		let ensFactory = await deployContract(ENSFactory, owner)
-  		let ensInstance = await ensFactory.methods.newENS(owner)
+
+  		let receipt = await ensFactory.methods.newENS('0xE8Aa2e7B68a7aa125ec5C8938647570811A91aE6').send({
+  			from: owner,
+  			gas: 90000000,
+  		})
+		 .on('transactionHash', function(hash){
+			console.log(hash)
+		})
+  		.on('receipt', function(receipt) {
+  			console.log(receipt)
+  			return receipt
+  		})
+  		.on('error', function(error) {
+  			console.error(error)
+  			process.exit()
+  		})
+
+  		console.log(receipt)
+  		let ensAddress = receipt.logs.filter(l => l.event == 'DeployENS')[0].args.ens
 
 		console.log('====================')
-		console.log('Deployed ENS:', ensInstance.address)
+		console.log('Deployed ENS:', ensAddress)
 
-		ens = new web3.eth.Contract(ENSFactory.abi, ensInstance.address)
+		ens = new web3.eth.Contract(ENSFactory.abi, ensAddress)
 	} else {
 		ens = ENS.at(ensAddress)
 	}
@@ -99,27 +134,66 @@ async function deploy() {
 	console.log('=========')
 	console.log('Deploying APM bases...')
 
-  // const apmRegistryBase = await APMRegistry.new()
-  // await logDeploy(apmRegistryBase, { verbose })
-  // const apmRepoBase = await Repo.new()
-  // await logDeploy(apmRepoBase, { verbose })
-  // const ensSubdomainRegistrarBase = await ENSSubdomainRegistrar.new()
-  // await logDeploy(ensSubdomainRegistrarBase, { verbose })
-
   	let apmRegistryBase = await deployContract(APMRegistry, owner)
   	let apmRepoBase = await deployContract(Repo, owner)
   	let ensSubdomainRegistrarBase = await deployContract(ENSSubdomainRegistrar, owner)
 
 	let daoFactory
 	if (daoFactoryAddress) {
-		//daoFactory = DAOFactory.at(daoFactoryAddress)
 		daoFactory = new web3.eth.Contract(DAOFactory.abi, daoFactoryAddress)
 		const hasEVMScripts = await daoFactory.regFactory() !== ZERO_ADDR
 		console.log(`Using provided DAOFactory (with${hasEVMScripts ? '' : 'out' } EVMScripts):`, daoFactoryAddress)
 	} else {
 		console.log('Deploying DAOFactory with EVMScripts...')
 		//daoFactory = await deployDaoFactory(owner)//(await deployDaoFactory(null, { artifacts, withEvmScriptRegistryFactory: true, verbose: false })).daoFactory
+		daoFactory = (await deployDaoFactory(owner)).daoFactory
 	}
+
+	console.log('Deploying APMRegistryFactory...')
+	const apmFactory = await deployContract(APMRegistryFactory, owner, 
+		[daoFactory.options.address,
+		apmRegistryBase.options.address,
+		apmRepoBase.options.address,
+		ensSubdomainRegistrarBase.options.address,
+		ensAddress,
+		'0x00']
+	)
+
+  console.log(`Assigning ENS name (${labelName}.${tldName}) to factory...`)
+
+  if (await ens.methods.owner(apmNode) === accounts[0]) {
+    console.log('Transferring name ownership from deployer to APMRegistryFactory')
+    await ens.methods.setOwner(apmNode, apmFactory.options.address)
+  } else {
+    log('Creating subdomain and assigning it to APMRegistryFactory')
+    try {
+      await ens.methods.setSubnodeOwner(tldHash, labelHash, apmFactory.options.address)
+    } catch (err) {
+      console.error(
+        `Error: could not set the owner of '${labelName}.${tldName}' on the given ENS instance`,
+        `(${ensAddress}). Make sure you have ownership rights over the subdomain.`
+      )
+      process.exit();
+    }
+  }
+
+	console.log('Deploying APM...')
+	const receipt = await apmFactory.methods.newAPM(tldHash, labelHash, owner)
+
+	console.log('=========')
+	const apmAddr = receipt.logs.filter(l => l.event == 'DeployAPM')[0].args.apm
+	console.log('# APM:')
+	console.log('Address:', apmAddr)
+	console.log('Transaction hash:', receipt.tx)
+	console.log('=========')
+
+	console.log("Done!")
+
+    return {
+      apmFactory,
+      ens,
+      apm: APMRegistry.at(apmAddr),
+    }
 }
 
 deploy()
